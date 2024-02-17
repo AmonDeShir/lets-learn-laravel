e44c7f4f50de3a691adbacf32c6a207dc502e9f1
Amon De Shir <amonsir29@gmail.com>
2024-02-17 22:40:05 +0100

    move development env to docker/development

--- docker/development/php/entrypoint.sh
+++ docker/development/php/entrypoint.sh
@@ -3,10 +3,9 @@
 cd /app
 
 composer require predis/predis
-composer install
 
 php artisan key:generate
-php artisan cache:clear 
+php artisan cache:clear
 php artisan config:clear
 
 chown -R www-data:www-data /app/storage
