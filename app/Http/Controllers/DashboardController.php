25943fca0fd7b289fe212c42eee8751577fe7cae
Amon De Shir <amonsir29@gmail.com>
2024-02-18 03:00:34 +0100

    add more stuff to blade files

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,21 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view("dashboard");
+        $users = [
+            [
+                "name" => "Alex",
+                "age" => 30,
+            ],
+            [
+                "name" => "Amon",
+                "age" => 23,
+            ],
+            [
+                "name" => "John",
+                "age" => 15,
+            ],
+        ];
+
+        return view("dashboard", ["users" => $users]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,13 @@
-<h1>Hello World</h1>
+@foreach ($users as $user)
+    <h1>{{$user['name']}}</h1>
+    <h3>is {{$user['age']}} years old</h3>
+
+    @if ($user['age'] < 18)
+        <h3 style='color: red'>and can't drive! What a <b>brat</b> XD</h3>
+    @endif
+
+    <hr>
+@endforeach
+
+
+@copyright {{ date('Y') }}
